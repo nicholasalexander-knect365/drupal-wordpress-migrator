53c27ce226f40effbe32f038f37865fe01bc8605
Nicholas Alexander <nicholas.alexander@knect365.com>
2018-03-16 10:25:23 +0000

    version map in index

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+// Drupal to Wordpres Migrator
+// by Nicholas Alexander
+// for Knect365.com
+// this is not a web app but it may become one
+?>
+<html>
+<h1>Drupal to Wordpress Migrator</h1>
+<p><strong>Welcome to the Drupal to Wordpress Migrator</strong></p>
+
+<p>Versions add new content elements.  Each version produces a wordpress site that has content.</p>
+<p>Version 0.1</p>
+<ol>
+<li>Use FG plugin to import nodes</li>
+</ol>
+
+<p>Version 0.2</p>
+<li>Run migrator.php on command line to build taxonomies </li>
+<ol>
+</ol>
+
+<p>Version 0.3</p>
+<ol>
+<li>Run contentyper.php on command line to build Content Types (Events, Podcasts, etc) as ACF entities</li>
+<i>or</i>
+<li>Convert wp_postmeta data into ACF from the FG import</li>
+</ol>
+<p>Version 0.4</p>
