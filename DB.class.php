83cdcf23e9fa47d041f22d0bd90e4301886d7d4c
Nicholas Alexander <nicholas.alexander@knect365.com>
2018-04-06 07:12:31 +0000

    fix parameter

--- DB.class.php
+++ DB.class.php
@@ -101,7 +101,7 @@ class DB {
 			// if the rowCount < 1 
 			// and it is a DELETE of an empty table
 			// the assert will be a problem...
-			if (!strncmp('DELETE FROM', $sql)) {
+			if (!strncmp('DELETE FROM', $sql, 11)) {
 				assert($rowCount > 0);
 			}
 
