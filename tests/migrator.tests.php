fa5bd33fb4852663f949056ccbc96cf47f3c5dd5
Nicholas Alexander <nicholas.alexander@knect365.com>
2018-04-10 11:29:57 +0100

    more unit tests

--- tests/migrator.tests.php
+++ tests/migrator.tests.php
@@ -10,19 +10,37 @@ class MigratorTest extends TestCase {
 	
 	//use TestCaseTrait;
 
+	private $db;
+	private $wp;
+	private $d7;
+
 	public function testDirectories() 
 	{
 		$this->assertDirectoryExists('images');
 		$this->assertDirectoryIsWritable('images');
 	}
 	
+	private function connectDB() 
+	{
+		$this->wp = new DB('local', 'wp');
+		$this->d7 = new DB('local', 'd7');
+		$this->assertObjectHasAttribute('connection', $this->d7);
+		$this->assertObjectHasAttribute('connection', $this->wp);
 
+	}
 	public function testDB()
 	{
-		$db = new DB('local');
-		$db->connect('wp');
-		$db->query('SHOW tables');
-		$records = $db->getRecords();
+		$this->connectDB();
+		$this->wp->query('SHOW tables');
+		$records = $this->wp->getRecords();
 		$this->assertGreaterThan(0, count($records));
+	}
+
+	public function testContent() 
+	{
+		$this->connectDB();
+		$record = $this->wp->record("SELECT COUNT(*) as c FROM wp_posts");
+		var_dump($record);
+		$this->assertGreaterThan(0, $record->c);
 	}
 }
