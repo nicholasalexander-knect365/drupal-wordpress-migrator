cf8853bcdd836fb155e42a5d0bbd10958f6d6d01
Nicholas Alexander <nicholas.alexander@knect365.com>
2018-05-22 07:57:35 +0000

    usermeta progress

--- User.class.php
+++ User.class.php
@@ -233,7 +233,7 @@ class User {
 		}
 
 		$clearUserMeta = true;
-		$sqlremove = "DELETE FROM wp_usermeta WHERE user_id=%d AND meta_key='%s'";
+		$sqlremove = "DELETE FROM wp_usermeta WHERE user_id=%d";// AND meta_key='%s'";
 
 		$sqlinsertfmt = "INSERT INTO wp_usermeta (user_id, meta_key, meta_value) VALUES (%d, '%s', '%s')";
 		$sqlupdatefmt = "UPDATE wp_usermeta SET meta_value='%s' WHERE user_id=%d AND meta_key='%s' LIMIT 1";
