9ff6bc2481d34534460f4d976cc2a0f3478ba0f2
Nicholas Alexander <nicholas.alexander@knect365.com>
2018-07-18 17:33:10 +0100

    read some records

--- fixes/userTests.php
+++ fixes/userTests.php
@@ -22,6 +22,7 @@ $db2 = new mysqli(
 			$credentials['database']);
 
 $credentials = ['host' => 'mysql', 'username' => 'test3_tele_com', 'password' => '6ORjLxbk9I8hs7tGwMFE', 'database' => 'test3_telecoms_com'];
+
 $db3 = new mysqli(
 			$credentials['host'],
 			$credentials['username'],
@@ -39,3 +40,17 @@ $record3 = $result->fetch_object();
 print "\nRecord counts : test2=".$record2->c;
 print "\nRecord counts : test3=".$record3->c;
 
+print "\n";
+
+$sql = "SELECT * FROM test2_telecoms_com.wp_users ORDER BY id DESC LIMIT 200";
+$result = $db2->query($sql);
+$records2 = $result->fetch_array();
+
+
+$sql = "SELECT * FROM test3_telecoms_com.wp_users ORDER BY id DESC LIMIT 200";
+$result = $db3->query($sql);
+$records3 = $result->fetch_array();
+
+foreach($record2 as $rec) {
+	debug($rec);
+}
